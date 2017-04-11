0e1506736b874f24dc2e411a1ef7953a1583c2c5
exoter <exoter@esa.int>
2017-04-11 13:21:56 +0200

    Fixed bug in dem_generation (wrong type)

--- src/dem_generation.cpp
+++ src/dem_generation.cpp
@@ -322,7 +322,7 @@ void DEM::mapTexture2MeshUVnew (pcl::TextureMesh &tex_mesh, pcl::TexMaterial &te
 	for (size_t m = 0; m < tex_mesh.tex_polygons.size (); ++m)
 	{
 		// texture coordinates for each mesh
-		std::vector<Eigen::Vector2f> texture_map_tmp;
+		std::vector<Eigen::Vector2f, Eigen::aligned_allocator<Eigen::Vector2f> > texture_map_tmp;
 		
 		// processing for each face
 		for (size_t i = 0; i < tex_mesh.tex_polygons[m].size (); ++i)
